6311321b02f6ea7145ed83e243ac7a4289ff303a
Ethan Frey <ethanfrey@users.noreply.github.com>
2017-08-30 20:22:17 +0200

    moved counter to examples dir

---
+++ examples/counter/cmd/counter/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+
+	"github.com/tendermint/tmlibs/cli"
+
+	client "github.com/cosmos/cosmos-sdk/client/commands"
+	"github.com/cosmos/cosmos-sdk/cmd/basecoin/commands"
+	"github.com/cosmos/cosmos-sdk/examples/counter/plugins/counter"
+)
+
+func main() {
+	var RootCmd = &cobra.Command{
+		Use:   "counter",
+		Short: "demo plugin for basecoin",
+	}
+
+	// TODO: register the counter here
+	commands.Handler = counter.NewHandler("mycoin")
+
+	RootCmd.AddCommand(
+		commands.InitCmd,
+		commands.StartCmd,
+		commands.UnsafeResetAllCmd,
+		client.VersionCmd,
+	)
+
+	cmd := cli.PrepareMainCmd(RootCmd, "CT", os.ExpandEnv("$HOME/.counter"))
+	cmd.Execute()
+}
