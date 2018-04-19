rd-alex-alex2006hw/cosmos-sdk

WIP: Track validator height (closes #582)

x/stake/keeper.go
@@ -95,7 +95,7 @@ func (k Keeper) setCandidate(ctx sdk.Context, candidate Candidate) {
 	store.Set(GetCandidateKey(candidate.Address), bz)
 
 	// mashal the new validator record
-	validator := Validator{address, candidate.Assets}
+	validator := candidate.validator()
 	bz, err = k.cdc.MarshalBinary(validator)
 	if err != nil {
 		panic(err)
@@ -108,9 +108,9 @@ func (k Keeper) setCandidate(ctx sdk.Context, candidate Candidate) {
 
 	// update the list ordered by voting power
 	if oldFound {
-		store.Delete(GetValidatorKey(address, oldCandidate.Assets, k.cdc))
+		store.Delete(GetValidatorKey(address, oldCandidate.Assets, oldCandidate.ValidatorHeight, k.cdc))
 	}
-	store.Set(GetValidatorKey(address, validator.VotingPower, k.cdc), bz)
+	store.Set(GetValidatorKey(address, validator.VotingPower, validator.Height, k.cdc), bz)
 
 	// add to the validators to update list if is already a validator
 	// or is a new validator
@@ -139,14 +139,14 @@ func (k Keeper) removeCandidate(ctx sdk.Context, address sdk.Address) {
 	// delete the old candidate record
 	store := ctx.KVStore(k.storeKey)
 	store.Delete(GetCandidateKey(address))
-	store.Delete(GetValidatorKey(address, oldCandidate.Assets, k.cdc))
+	store.Delete(GetValidatorKey(address, oldCandidate.Assets, oldCandidate.ValidatorHeight, k.cdc))
 
 	// delete from recent and power weighted validator groups if the validator
 	// exists and add validator with zero power to the validator updates
 	if store.Get(GetRecentValidatorKey(address)) == nil {
 		return
 	}
-	bz, err := k.cdc.MarshalBinary(Validator{address, sdk.ZeroRat})
+	bz, err := k.cdc.MarshalBinary(Validator{address, sdk.ZeroRat, uint64(0)})
 	if err != nil {
 		panic(err)
 	}
@@ -203,7 +203,7 @@ func (k Keeper) GetValidators(ctx sdk.Context) (validators []Validator) {
 	iterator = store.Iterator(subspace(ToKickOutValidatorsKey))
 	for ; iterator.Valid(); iterator.Next() {
 		addr := AddrFromKey(iterator.Key())
-		bz, err := k.cdc.MarshalBinary(Validator{addr, sdk.ZeroRat})
+		bz, err := k.cdc.MarshalBinary(Validator{addr, sdk.ZeroRat, uint64(0)})
 		if err != nil {
 			panic(err)
 		}

InsChain/inschain-public

x/stake/keeper.go
@@ -101,9 +101,9 @@ func (k Keeper) setCandidate(ctx sdk.Context, candidate Candidate) {
 
 	// update the list ordered by voting power
 	if oldFound {
-		store.Delete(GetValidatorKey(address, oldCandidate.Assets, k.cdc))
+		store.Delete(GetValidatorKey(address, oldCandidate.Assets, oldCandidate.ValidatorHeight, k.cdc))
 	}
-	store.Set(GetValidatorKey(address, validator.Power, k.cdc), bz)
+	store.Set(GetValidatorKey(address, validator.Power, validator.Height, k.cdc), bz)
 
 	// add to the validators to update list if is already a validator
 	// or is a new validator
@@ -136,7 +136,7 @@ func (k Keeper) removeCandidate(ctx sdk.Context, address sdk.Address) {
 	// delete the old candidate record
 	store := ctx.KVStore(k.storeKey)
 	store.Delete(GetCandidateKey(address))
-	store.Delete(GetValidatorKey(address, candidate.Assets, k.cdc))
+	store.Delete(GetValidatorKey(address, candidate.Assets, candidate.ValidatorHeight, k.cdc))
 
 	// delete from recent and power weighted validator groups if the validator
 	// exists and add validator with zero power to the validator updates