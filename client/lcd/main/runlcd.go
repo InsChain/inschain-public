8952d66957f9aee3ae9504bda87646d0e125d453
taoyahui <taoyahui>
2018-05-04 21:42:35 +0800

    delete lcd/impl lcd/main

--- client/lcd/main/runlcd.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "github.com/cosmos/cosmos-sdk/client/lcd/impl"
-
-
-func main() {
-	impl.StartLCDSpecify()
-}
-
--- client/lcd/main/runtm.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import  f "github.com/cosmos/cosmos-sdk/client/lcd/impl"
-func main() {
-
-	f.StartTMAndLCD()
-}
